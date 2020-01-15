4a7ab8ddb459290cd9df0cbcfdf58bdfb01b8ace
Radek Jirsa <jirsa.r@hanaboso.com>
2020-01-15 14:08:00 +0100

    StaticClassTraitTest: removed bad @cover

--- tests/Unit/Traits/StaticClassTraitTest.php
+++ tests/Unit/Traits/StaticClassTraitTest.php
@@ -14,12 +14,14 @@ final class StaticClassTraitTest extends TestCase
 {
 
     /**
-     * @covers \UtilsTests\Unit\Traits\TestStaticTraits::__constructor
+     *
      */
     public function testStaticClass(): void
     {
         self::expectException(LogicException::class);
-        self::expectExceptionMessage(sprintf('Class %s is static and cannot be instantiated.', TestStaticTraits::class));
+        self::expectExceptionMessage(
+            sprintf('Class %s is static and cannot be instantiated.', TestStaticTraits::class)
+        );
 
         new TestStaticTraits();
     }
