6eb8f73c364fefe5df293b9803fd556ccae20cba
Radek Jirsa <jirsa.r@hanaboso.com>
2020-04-20 14:29:44 +0200

    Composer updated

--- src/String/Json.php
+++ src/String/Json.php
@@ -2,6 +2,8 @@
 
 namespace Hanaboso\Utils\String;
 
+use JsonException;
+
 /**
  * Class Json
  *
@@ -12,22 +14,25 @@ final class Json
 
     /**
      * @param mixed $data
+     * @param int   $options
      *
      * @return string
      */
-    public static function encode($data): string
+    public static function encode($data, int $options = JSON_THROW_ON_ERROR): string
     {
-        return json_encode($data, JSON_THROW_ON_ERROR);
+        return json_encode($data, $options) ?: '';
     }
 
     /**
      * @param string $data
+     * @param int    $options
      *
      * @return mixed[]
+     * @throws JsonException
      */
-    public static function decode(string $data): array
+    public static function decode(string $data, int $options = JSON_THROW_ON_ERROR): array
     {
-        return (array) json_decode($data, TRUE, 512, JSON_THROW_ON_ERROR);
+        return (array) json_decode($data, TRUE, 512, $options);
     }
 
 }
