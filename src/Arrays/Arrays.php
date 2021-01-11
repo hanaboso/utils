79af890c6374e634e2c14452280f34c7d35674c3
Radek Bruha <bruha.r@hanaboso.com>
2021-01-11 12:23:03 +0100

    Arrays: Add diff

--- src/Arrays/Arrays.php
+++ src/Arrays/Arrays.php
@@ -20,4 +20,74 @@ final class Arrays
         return is_array($value) && (!$value || array_keys($value) === range(0, count($value) - 1));
     }
 
+    /**
+     * @see https://github.com/charliekassel/array-diff/blob/master/src/ArrayDiff.php
+     *
+     * @param mixed[] $old
+     * @param mixed[] $new
+     *
+     * @return mixed[]
+     */
+    public static function diff(array $old, array $new): array
+    {
+        return [
+            'created' => self::getCreatedKeys($old, $new),
+            'updated' => self::getUpdatedKeys($old, $new),
+            'deleted' => self::getRemovedKeys($old, $new),
+        ];
+    }
+
+    /**
+     * @param mixed[] $old
+     * @param mixed[] $new
+     *
+     * @return mixed[]
+     */
+    private static function getCreatedKeys(array $old, array $new): array
+    {
+        return array_filter($new, static fn($key): bool => !array_key_exists($key, $old), ARRAY_FILTER_USE_KEY);
+    }
+
+    /**
+     * @param mixed[] $old
+     * @param mixed[] $new
+     *
+     * @return mixed[]
+     */
+    private static function getRemovedKeys(array $old, array $new): array
+    {
+        return array_filter($old, static fn($key): bool => !array_key_exists($key, $new), ARRAY_FILTER_USE_KEY);
+    }
+
+    /**
+     * @param mixed[] $old
+     * @param mixed[] $new
+     *
+     * @return mixed[]
+     */
+    private static function getUpdatedKeys(array $old, array $new): array
+    {
+        $changed = array_filter(
+            $new,
+            static fn($newItem, $key): bool => array_key_exists($key, $old) && $old[$key] !== $newItem,
+            ARRAY_FILTER_USE_BOTH
+        );
+
+        array_walk(
+            $changed,
+            static function (&$changedItem, $key) use ($old): void {
+                if (is_array($changedItem) && !is_null($old[$key])) {
+                    $changedItem = self::diff($old[$key], $changedItem);
+                } else {
+                    $changedItem = [
+                        'old' => $old[$key],
+                        'new' => $changedItem,
+                    ];
+                }
+            }
+        );
+
+        return $changed;
+    }
+
 }
